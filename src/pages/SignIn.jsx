dceb48622dd2611f08f5ee6d39944056330e56cb
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-01-18 16:08:37 +0600

    User Signin done by ReduxToolkit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
 
 const Container = styled.div`
   display: flex;
@@ -68,13 +70,16 @@ const SignIn = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const dispatch = useDispatch()
   const handleLogin = async (e) => {
     e.preventDefault()
+    dispatch(loginStart())
     try {
       const res = await axios.post("/auth/signin", {name, password})
-      console.log(res.data)
+      // console.log(res.data)
+      dispatch(loginSuccess(res.data))
     } catch (err) {
-      
+      dispatch(loginFailure())
     }
   }
   return (
